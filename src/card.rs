d06ce9e8a652bce4ff9adebf6679b6498e814dd2
Macuyler Dunn <dev@macuyler.com>
2024-01-01 17:54:21 -0500

    Add Card::invalid constructor

--- src/card.rs
+++ src/card.rs
@@ -48,17 +48,29 @@ impl Card {
     pub fn create(value: Value, suit: Suit) -> Self {
         Card::new(value as u8, suit as u8)
     }
+
+    /// Get an invalid card
+    pub fn invalid() -> Self {
+        Card::create(Value::Invalid, Suit::Clubs)
+    }
 }
 
 impl From<u8> for Card {
     fn from(id: u8) -> Self {
-        Card::new((id % 13) + 1, id / 13)
+        match id {
+            52.. => Card::invalid(),
+            _ => Card::new((id % 13) + 1, id / 13),
+        }
     }
 }
 
 impl From<Card> for u8 {
     fn from(c: Card) -> Self {
-        (c.suit * 13) + c.value - 1
+        if c.value == Value::Invalid as u8 {
+            52
+        } else {
+            (c.suit * 13) + c.value - 1
+        }
     }
 }
 
@@ -78,44 +90,56 @@ mod tests {
 
     #[test]
     fn test_card_from() {
-        // Ace of Spades is id 39
-        let id: u8 = 39; // 13 * 3 + 0
-        assert_eq!(Card::from(id), Card::create(Value::Ace, Suit::Spades));
+        // Ace of Clubs is id 0
+        let id = 0; // 13 * 0 + 0
+        assert_eq!(Card::from(id), Card::create(Value::Ace, Suit::Clubs));
 
         // Two of Spades is id 40
-        let id: u8 = 40; // 13 * 3 + 1
+        let id = 40; // 13 * 3 + 1
         assert_eq!(Card::from(id), Card::create(Value::Two, Suit::Spades));
 
         // Ten of Diamonds is id 22
-        let id: u8 = 22; // 13 * 1 + 9
+        let id = 22; // 13 * 1 + 9
         assert_eq!(Card::from(id), Card::create(Value::Ten, Suit::Diamonds));
 
         // Seven of Clubs is id 6
-        let id: u8 = 6; // 13 * 0 + 6
+        let id = 6; // 13 * 0 + 6
         assert_eq!(Card::from(id), Card::create(Value::Seven, Suit::Clubs));
 
         // Queen of Hearts is id 37
-        let id: u8 = 37; // 13 * 2 + 11
+        let id = 37; // 13 * 2 + 11
         assert_eq!(Card::from(id), Card::create(Value::Queen, Suit::Hearts));
+
+        // King of Spades is id 51
+        let id = 51; // 13 * 3 + 12
+        assert_eq!(Card::from(id), Card::create(Value::King, Suit::Spades));
+
+        // 52 and above are invalid
+        let id = 52; // Invalid!
+        assert_eq!(Card::from(id), Card::create(Value::Invalid, Suit::Clubs));
     }
 
     #[test]
     fn test_card_to_id() {
         // King of Clubs is id 12
-        let id: u8 = 12; // 13 * 0 + 12
+        let id = 12; // 13 * 0 + 12
         assert_eq!(u8::from(Card::create(Value::King, Suit::Clubs)), id);
 
         // Five of Diamonds is id 17
-        let id: u8 = 17; // 13 * 1 + 4
+        let id = 17; // 13 * 1 + 4
         assert_eq!(u8::from(Card::create(Value::Five, Suit::Diamonds)), id);
 
         // Eight of Hearts is id 33
-        let id: u8 = 33; // 13 * 2 + 7
+        let id = 33; // 13 * 2 + 7
         assert_eq!(u8::from(Card::create(Value::Eight, Suit::Hearts)), id);
 
         // Jack of Spades is id 49
-        let id: u8 = 49; // 13 * 3 + 10
+        let id = 49; // 13 * 3 + 10
         assert_eq!(u8::from(Card::create(Value::Jack, Suit::Spades)), id);
+
+        // Invalid is id 52
+        let id = 52;
+        assert_eq!(u8::from(Card::create(Value::Invalid, Suit::Clubs)), id);
     }
 
     #[test]
