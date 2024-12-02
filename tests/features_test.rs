playsuipi/core

commit e10b38e5a9906b0b16b763888a498d60dfca40ae

Update InvalidHandAddressPosition rule
We can allow a hand address to be the move destination so long as we do
an implicit discard anytime that happens. This allows for the hand
address to appear as the first address in an annotation. That being
said, the hand address still needs to be last for pair moves.

Implementing this requires an implicit `collapse_floor` call inside of
the `apply_move` function. This is a breaking API change, but it is
needed for more than just the new hand address flexibility. In order to
properly implement a game of Suipi, you must collapse the floor after
every single move is applied. Allowing developers to skip this step can
result in a broken game.

src/state.rs
@@ -157,7 +157,7 @@ impl State {
     }
 
     /// Collapse all piles to the beginning of the floor array
-    pub fn collapse_floor(&mut self) {
+    fn collapse_floor(&mut self) {
         self.floor.retain(|x| !x.is_empty());
         while self.floor.len() < 13 {
             self.floor.push(Pile::empty());
@@ -351,31 +351,33 @@ impl State {
     /// Apply a move to the game state
     pub fn apply(&mut self, m: Move) -> Result<(), StateError> {
         m.is_valid()?;
-        if m.actions.len() == 1 {
-            self.discard(m.actions[0].address)?;
-        } else {
-            let mut builds = vec![];
-            for w in m.actions.windows(2).rev() {
-                match w[1].operation {
-                    Operation::Passive => {
-                        builds.push(w[1].address);
-                    }
-                    Operation::Active => {
-                        self.build(w[0].address, w[1].address)?;
-                    }
+        let mut builds = vec![];
+        for w in m.actions.windows(2).rev() {
+            match w[1].operation {
+                Operation::Passive => {
+                    builds.push(w[1].address);
+                }
+                Operation::Active => {
+                    self.build(w[0].address, w[1].address)?;
                 }
             }
-            let destination = m.actions[0].address;
-            let pair = m.actions[0].operation == Operation::Active;
-            for (i, b) in builds.iter().rev().enumerate() {
-                if i == builds.len() - 1 && pair {
-                    self.pair(destination, b.to_owned())?;
-                } else {
-                    self.group(destination, b.to_owned())?;
-                }
-            }
-            self.validate_turn(destination, pair)?;
         }
+        let destination = m.actions[0].address;
+        let pair = m.actions[0].operation == Operation::Active;
+        for (i, b) in builds.iter().rev().enumerate() {
+            if i == builds.len() - 1 && pair {
+                self.pair(destination, b.to_owned())?;
+            } else {
+                self.group(destination, b.to_owned())?;
+            }
+        }
+        self.collapse_floor();
+        if let Address::Hand(_) = destination {
+            if !pair {
+                self.discard(destination)?;
+            }
+        }
+        self.validate_turn(destination, pair)?;
         Ok(())
     }
 }
@@ -506,8 +508,8 @@ mod tests {
             [
                 single(Value::Four, Suit::Clubs),
                 single(Value::Seven, Suit::Diamonds),
-                empty(),
                 single(Value::Eight, Suit::Clubs),
+                empty(),
                 empty(),
                 empty(),
                 empty(),
@@ -667,55 +669,6 @@ mod tests {
                 single(Value::Two, Suit::Spades),
                 single(Value::Eight, Suit::Clubs),
                 single(Value::Ace, Suit::Hearts),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty()
-            ]
-        );
-    }
-
-    #[test]
-    fn test_collapse_floor_method() {
-        let mut g = setup();
-
-        assert!(g.build(Address::Floor(0), Address::Hand(7)).is_ok());
-        assert!(g.group(Address::Floor(0), Address::Floor(1)).is_ok());
-        assert!(g.pair(Address::Floor(0), Address::Hand(4)).is_ok());
-
-        assert_eq!(
-            g.floor,
-            [
-                empty(),
-                empty(),
-                single(Value::Two, Suit::Spades),
-                single(Value::Eight, Suit::Clubs),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty(),
-                empty()
-            ]
-        );
-
-        g.collapse_floor();
-
-        assert_eq!(
-            g.floor,
-            [
-                single(Value::Two, Suit::Spades),
-                single(Value::Eight, Suit::Clubs),
-                empty(),
-                empty(),
-                empty(),
                 empty(),
                 empty(),
                 empty(),

tests/features_test.rs
@@ -14,8 +14,8 @@ fn test_pair_two_cards() {
         vec![
             single(Value::Four, Suit::Clubs),
             single(Value::Seven, Suit::Diamonds),
-            empty(), // single(Value::Two, Suit::Spades),
             single(Value::Eight, Suit::Clubs),
+            empty(), // single(Value::Two, Suit::Spades),
             empty(),
             empty(),
             empty(),
@@ -105,7 +105,6 @@ fn test_build_and_group() {
         g.floor,
         [
             single(Value::Four, Suit::Clubs),
-            empty(), // single(Value::Seven, Suit::Diamonds),
             single(Value::Two, Suit::Spades),
             group(
                 vec![
@@ -115,6 +114,7 @@ fn test_build_and_group() {
                 ],
                 Value::Eight
             ), // single(Value::Eight, Suit::Clubs),
+            empty(), // single(Value::Seven, Suit::Diamonds),
             empty(),
             empty(),
             empty(),
@@ -204,9 +204,9 @@ fn test_build_and_pair() {
         g.floor,
         [
             single(Value::Ten, Suit::Hearts),
+            single(Value::Jack, Suit::Diamonds),
             empty(), // single(Value::Four, Suit::Clubs),
             empty(), // single(Value::Five, Suit::Diamonds),
-            single(Value::Jack, Suit::Diamonds),
             empty(),
             empty(),
             empty(),

src/action.rs
@@ -46,7 +46,7 @@ impl fmt::Display for MoveError {
                 MoveError::InvalidHandAddressCount =>
                     "You must use exactly one hand address in your move",
                 MoveError::InvalidHandAddressPosition =>
-                    "The hand address may not be the first address in your move",
+                    "The hand address must be the last address in your move",
             }
         )
     }
@@ -146,10 +146,12 @@ impl Move {
             != 1
         {
             Err(MoveError::InvalidHandAddressCount)
-        } else if self.actions.len() > 1 {
-            match self.actions.first().unwrap().address {
-                Address::Hand(_) => Err(MoveError::InvalidHandAddressPosition),
-                Address::Floor(_) => Ok(()),
+        } else if !self.actions.is_empty()
+            && self.actions.first().unwrap().operation == Operation::Active
+        {
+            match self.actions.last().unwrap().address {
+                Address::Hand(_) => Ok(()),
+                Address::Floor(_) => Err(MoveError::InvalidHandAddressPosition),
             }
         } else {
             Ok(())
@@ -362,7 +364,7 @@ mod tests {
 
         assert_eq!(
             Move::new(vec![
-                Action::new(Operation::Passive, Address::Hand(0)),
+                Action::new(Operation::Active, Address::Hand(0)),
                 Action::new(Operation::Passive, Address::Floor(0)),
             ])
             .is_valid(),

src/game.rs
@@ -37,8 +37,6 @@ impl Game {
         }
         // Toggle turn
         self.state.turn = self.state.dealer.card_count() > self.state.opponent.card_count();
-        // Cleanup floor
-        self.state.collapse_floor();
         // Handle end of round
         if self.state.dealer.card_count() == 0 && self.state.opponent.card_count() == 0 {
             // Handle end of game

tests/common/mod.rs
@@ -47,7 +47,6 @@ pub fn apply(g: &mut State, x: &str) -> Result<(), StateError> {
 pub fn apply_moves(g: &mut State, xs: Vec<&str>) {
     for x in xs {
         assert!(apply(g, x).is_ok());
-        g.collapse_floor();
         g.turn = !g.turn;
     }
 }

tests/rules_test.rs
@@ -109,9 +109,9 @@ fn test_cannot_skip_hand_address() {
 }
 
 #[test]
-fn test_cannot_continue_after_hand_address() {
+fn test_cannot_start_pair_with_hand_address() {
     let mut g = setup_default();
-    let res = apply(&mut g, "3+A+C&D");
+    let res = apply(&mut g, "*5&C");
     assert!(res.is_err());
     assert_eq!(
         res.err().unwrap(),

commit 692624b791f2f6c4d69017781e784cd0b91261cb

Fix `collapse_floor` stacking destination bug
When stacking two floor piles, the `collapse_floor` call towards the end
of the `apply` method can shift the destination address's value before
`validate_turn` is called.

This bug probably happens all the time. But it only causes issues if the
new value of the destination address is not available in your hand. In
that specific case we get a `StateError::UnpairablePileValue` result.
Marking the turn as invalid.

This bug was introduced by e10b38e, which added the ability to use a
hand address as the destination of a move. We handle this behavior by
doing an implicit discard at the end of the move. In order to properly
handle that `discard` call, we must first call `collapse_floor`.

To mitigate this issue we can move the main `collapse_floor` call to run
after `validate_turn`. This allows the `validate_turn` call to run with
an un-collapsed floor state. Then we will need to add a secondary
`collapse_floor` call immediately before the implicit discard of hand
address destinations. When the destination is a hand address, the check
for `StateError::UnpairablePileValue` is essentially a no-op. There is
never a case where a hand address's value is not included in the
player's hand. So in that case it is okay to call `collapse_floor`
before we run `validate_turn`.

src/state.rs
@@ -368,13 +368,14 @@ impl State {
                 self.group(destination, b.to_owned())?;
             }
         }
-        self.collapse_floor();
         if let Address::Hand(_) = destination {
             if !pair {
+                self.collapse_floor();
                 self.discard(destination)?;
             }
         }
         self.validate_turn(destination, pair)?;
+        self.collapse_floor();
         Ok(())
     }
 }

tests/features_test.rs
@@ -173,6 +173,138 @@ fn test_build_and_group() {
 }
 
 #[test]
+fn test_build_and_group_with_build() {
+    let mut g = setup([
+        222, 29, 61, 3, 160, 4, 192, 251, 244, 132, 175, 198, 124, 182, 184, 25, 115, 128, 175,
+        188, 165, 160, 176, 189, 23, 178, 49, 163, 86, 158, 145, 248,
+    ]);
+
+    apply_moves(
+        &mut g,
+        vec![
+            "*B&4", "*B&4", "*B&8", "6", "*B&2", "2", "1", "*C&3", "7", "C+8", "5", "C&B+1",
+        ],
+    );
+
+    assert_eq!(
+        read_floor(&g),
+        vec![
+            single(Value::Queen, Suit::Spades),
+            group(
+                vec![
+                    card(Value::Four, Suit::Diamonds),
+                    card(Value::Six, Suit::Hearts),
+                    card(Value::Nine, Suit::Diamonds),
+                    card(Value::Ace, Suit::Diamonds),
+                ],
+                Value::Ten
+            ), // single(Value::Jack, Suit::Hearts),
+            single(Value::Jack, Suit::Spades), // single(Value::Six, Suit::Diamonds),
+            empty(),                           // single(Value::Ten, Suit::Spades),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty()
+        ]
+    );
+
+    assert_eq!(
+        read_hands(&g),
+        vec![
+            // Opponent hand:
+            blank(), // card(Value::Seven, Suit::Spades),
+            blank(), // card(Value::King, Suit::Hearts),
+            card(Value::Ace, Suit::Hearts),
+            blank(), // card(Value::Jack, Suit::Diamonds),
+            blank(), // card(Value::Jack, Suit::Spades),
+            card(Value::Eight, Suit::Spades),
+            blank(), // card(Value::Four, Suit::Diamonds),
+            blank(), // card(Value::Ten, Suit::Clubs),
+            // Dealer hand:
+            blank(), // card(Value::Ace, Suit::Diamonds),
+            blank(), // card(Value::Nine, Suit::Diamonds),
+            blank(), // card(Value::Seven, Suit::Clubs),
+            blank(), // card(Value::Six, Suit::Clubs),
+            card(Value::Eight, Suit::Clubs),
+            blank(), // card(Value::King, Suit::Spades),
+            card(Value::Ten, Suit::Diamonds),
+            blank(), // card(Value::Six, Suit::Hearts),
+        ]
+    );
+}
+
+#[test]
+fn test_hand_address_destination_discard() {
+    let mut g = setup([
+        222, 29, 61, 3, 160, 4, 192, 251, 244, 132, 175, 198, 124, 182, 184, 25, 115, 128, 175,
+        188, 165, 160, 176, 189, 23, 178, 49, 163, 86, 158, 145, 248,
+    ]);
+
+    apply_moves(
+        &mut g,
+        vec![
+            "*B&4", "*B&4", "*B&8", "6", "*B&2", "2", "1", "*C&3", "7", "C+8", "5", "1+B&C",
+        ],
+    );
+
+    assert_eq!(
+        read_floor(&g),
+        vec![
+            single(Value::Queen, Suit::Spades),
+            single(Value::Jack, Suit::Spades), // single(Value::Jack, Suit::Hearts),
+            group(
+                vec![
+                    card(Value::Ace, Suit::Diamonds),
+                    card(Value::Nine, Suit::Diamonds),
+                    card(Value::Four, Suit::Diamonds),
+                    card(Value::Six, Suit::Hearts),
+                ],
+                Value::Ten
+            ), // single(Value::Six, Suit::Diamonds),
+            empty(),                           // single(Value::Ten, Suit::Spades),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty(),
+            empty()
+        ]
+    );
+
+    assert_eq!(
+        read_hands(&g),
+        vec![
+            // Opponent hand:
+            blank(), // card(Value::Seven, Suit::Spades),
+            blank(), // card(Value::King, Suit::Hearts),
+            card(Value::Ace, Suit::Hearts),
+            blank(), // card(Value::Jack, Suit::Diamonds),
+            blank(), // card(Value::Jack, Suit::Spades),
+            card(Value::Eight, Suit::Spades),
+            blank(), // card(Value::Four, Suit::Diamonds),
+            blank(), // card(Value::Ten, Suit::Clubs),
+            // Dealer hand:
+            blank(), // card(Value::Ace, Suit::Diamonds),
+            blank(), // card(Value::Nine, Suit::Diamonds),
+            blank(), // card(Value::Seven, Suit::Clubs),
+            blank(), // card(Value::Six, Suit::Clubs),
+            card(Value::Eight, Suit::Clubs),
+            blank(), // card(Value::King, Suit::Spades),
+            card(Value::Ten, Suit::Diamonds),
+            blank(), // card(Value::Six, Suit::Hearts),
+        ]
+    );
+}
+
+#[test]
 fn test_build_two_cards() {
     let mut g = setup([
         62, 136, 82, 123, 15, 88, 230, 198, 158, 233, 24, 104, 252, 215, 233, 118, 133, 47, 6, 62,